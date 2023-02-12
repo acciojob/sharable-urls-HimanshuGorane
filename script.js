8626e2fce3d3adc24600e6fc599c4ea9522c4128
HimanshuGorane <117897707+HimanshuGorane@users.noreply.github.com>
2023-02-12 09:31:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+documet.getElementById("button").addEventListener("click",function (event) {
+	event.preventDefault();
+	var name = document.getElementById("name").value;
+	var year = document.getElementById("year").value;
+	document.getElementById("url").innerHTML =
+	 "https://localhost:8080/?name=" + name + "&year=" + year;
+	
+});
